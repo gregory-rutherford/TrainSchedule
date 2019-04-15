9bcd921d822d0f5a3d45a992e2c35b34a0972507
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2019-04-15 08:35:14 -0500

    psuedo coded rest of logic, need correct moment methods

--- assets/app.js
+++ assets/app.js
@@ -18,14 +18,37 @@ $(document).on("click", "#submit", function() {
     event.preventDefault();
     var trainName = $("#name").val().trim();
     var trainDestination = $("#destination").val().trim();
-    //converts to military time
-    var trainTime = $("#time").val().trim();
+
+
+
+
+
+    //grabbing the user submitted time
+    var firstTrain = $("#time").val().trim();
+    console.log(firstTrain);
+    //stores the user submitted first train time to a coordinated universal time
+    var trainTime = moment.utc(firstTrain, "HH:mm");
+    console.log(trainTime);
+    //stores the user submitted train frequency in a variable
     var trainFreq = $("#frequency").val().trim();
+    console.log(trainFreq);
+
+    //adds the frequency to the first train time
+    trainTime.add(trainFreq, "minutes");
+    var updatedTime = trainTime.format("HH:mm");
+    console.log(updatedTime);
+    
+    //stores the current time in the matching format as the updated time
+    var currentTime = moment().format("HH:mm");
+    console.log(currentTime);
+    
+    //now you have to add the frequency until the next arrival time is over the current time, once it surpasses it stop adding, then do your calculation for minutes away.  
+
 
     database.ref().push({
         title: trainName,
         place: trainDestination,
-        beginTime: trainTime,
+        beginTime: firstTrain,
         frequency: trainFreq
     })
 })
@@ -33,15 +56,14 @@ $(document).on("click", "#submit", function() {
 
 function makeRow(response) {
   $("#trainDisplay").append(
-    `
-    <tr>
-      <td>${response.title}</td>,
-      <td>${response.place}</td>,
-      <td>${response.beginTime}</td>,
-      <td>${response.frequency}</td>,
+    ` <tr> 
+      <td>${response.title}</td>
+      <td>${response.place}</td>
+      <td>${response.beginTime}</td>
+      <td>${response.frequency}</td>
       </tr>
     `
-  )
+  );
 }
 
 
